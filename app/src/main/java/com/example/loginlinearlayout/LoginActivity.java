42a00fbf4321a90f9e06c3cc5ccd32f88679486f
alejandroagca <alejandroagca1995@gmail.com>
2017-10-09 10:33:33 +0200

    Final

--- app/src/main/java/com/example/loginlinearlayout/LoginActivity.java
+++ app/src/main/java/com/example/loginlinearlayout/LoginActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    /**
+     * LoginLinearLayout
+     * @author Alejandro Aguilar
+     * @version 1.0
+     * @see android.app.Activity
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
